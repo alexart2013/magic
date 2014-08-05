ef792baf0135070bd136459896a10d3b56a9da82
Alex Muravya <asm@virtalab.net>
2014-08-06 00:27:54 +0400

    Let the code begins

---
+++ src/main/java/ws/kyberorg/magic/Main.java
@@ -0,0 +1,13 @@
+package ws.kyberorg.magic;
+
+/**
+ * Application starts here
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello fair people of Java!" );
+    }
+}
---
+++ src/test/java/ws/kyberorg/magic/test/StubTest.java
@@ -0,0 +1,15 @@
+package ws.kyberorg.magic.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Stub UnitTest
+ */
+public class StubTest {
+    @Test
+    public void stub(){
+        //this project made just for fun and there is no serious unit testing
+        Assert.assertTrue(true);
+    }
+}
